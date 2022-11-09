54b434b1e7f49928d34dced4d7ca49b0e7e3a57e
gustavo Xavier <83915382+gustavoxavierf2@users.noreply.github.com>
2022-11-09 12:02:16 -0300

    firt commit

---
+++ chatBot.py
@@ -0,0 +1,24 @@
+# biblioteca - pip install chatterbot 
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+from spacy.cli import download
+download("en_core_web_sm")
+class ENGSM:
+    ISO_639_1 = 'en_core_web_sm'
+
+   
+bot = ChatBot("python", tagger_language=ENGSM)
+conversa = ['eae', 'blz?', 'tudobem?', 'eu estou bem']
+conversa2 = ['qual seu fime preferido?', 'de volta pro futuro']
+
+trainer = ListTrainer(bot)
+trainer.train(conversa)
+trainer.train(conversa2)
+
+
+while True:
+    resposta = bot.get_response(input('voce: '))
+    if float(resposta.confidence) > 0.7:
+        print('python: ', resposta)
+    else:
+        print('n tenho certeza')
